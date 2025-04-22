f57d266e3250c81f9e20116891c34018444caab2
Mark Allan Wentowski Jr <mark@MacBookPro.attlocal.net>
2025-04-22 09:39:25 -0400

    troubleshooting admin panel

--- admin-gitpod/vite.config.ts
+++ admin-gitpod/vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-const hostUrl = import.meta.env.VITE_REACT_APP_SERVER_URL;
+// Load .env variables
+dotenv.config();
+
+const hostUrl = process.env.VITE_REACT_APP_HOST_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: [`${hostUrl}`],
+    allowedHosts: hostUrl ? [hostUrl] : [],
   },
   base: "./",
 });
